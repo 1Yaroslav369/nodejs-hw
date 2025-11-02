b4fb0a2c909fab910e17aa49472460917247c19b
yaroslav kuzmin <kuzminyaroslav13@gmail.com>
2025-11-02 21:01:08 +0100

    eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,9 +5,10 @@ import { defineConfig } from "eslint/config";
 export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-    languageOptions: { globals: globals.node },
+    languageOptions: {
+      globals: globals.node, // <-- важливо для Node.js
+    },
+    extends: [js.configs.recommended],
     rules: {
       semi: "error",
       "no-unused-vars": ["error", { args: "none" }],
