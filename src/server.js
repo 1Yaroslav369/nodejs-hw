8dcf2e38279bc27ef29d57b607d05d96c7620b87
yaroslav kuzmin <kuzminyaroslav13@gmail.com>
2025-11-02 20:21:12 +0100

    add helmet

--- src/server.js
+++ src/server.js
@@ -2,10 +2,12 @@ import express from "express";
 import cors from "cors";
 import 'dotenv/config';
 import pino from 'pino-http';
+import helmet from "helmet";
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
+app.use(helmet());
 app.use(express.json());
 app.use(cors());
 app.use(
